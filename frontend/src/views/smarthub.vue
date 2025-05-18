143c5a7421affaf992a2fcf0f8ac344f38bc55ec
kioku <1830787294@qq.com>
2025-05-18 18:44:33 +0800

    初步实现视频播放功能

--- frontend/src/views/smarthub.vue
+++ frontend/src/views/smarthub.vue
@@ -4,6 +4,18 @@
         <!-- 这里可以添加页面2的内容 -->
         <WeatherWidget />
     </div>
+    <div class="bilibili-video">
+    <iframe 
+      :src="`//player.bilibili.com/player.html?bvid=${bvid}&page=1`"
+      scrolling="no"
+      border="0"
+      frameborder="no"
+      framespacing="0"
+      allowfullscreen="true"
+      width="100%"
+      height="500"
+    ></iframe>
+  </div>
 </template>
 
 <script>
@@ -17,6 +29,7 @@ export default {
     },
     data() {
         return {
+            bvid: "BV1Ts4y1872o"
         };
     }
 }
