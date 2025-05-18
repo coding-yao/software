acb2fbe7323a5b37e322867e9fdeeec1f1abb6d2
kioku <1830787294@qq.com>
2025-05-18 18:54:17 +0800

    完善布局

--- frontend/src/views/smarthub.vue
+++ frontend/src/views/smarthub.vue
@@ -1,20 +1,15 @@
 <template>
-    <NavBar current-page="smarthub" />
-    <div class="main-content">
-        <!-- 这里可以添加页面2的内容 -->
-        <WeatherWidget />
+  <NavBar current-page="smarthub" />
+  <div class="main-content">
+    <WeatherWidget />
+    <div class="video-container">
+      <iframe 
+        :src="`//player.bilibili.com/player.html?bvid=${bvid}&page=1`"
+        scrolling="no"
+        frameborder="no"
+        allowfullscreen="true"
+      ></iframe>
     </div>
-    <div class="bilibili-video">
-    <iframe 
-      :src="`//player.bilibili.com/player.html?bvid=${bvid}&page=1`"
-      scrolling="no"
-      border="0"
-      frameborder="no"
-      framespacing="0"
-      allowfullscreen="true"
-      width="100%"
-      height="500"
-    ></iframe>
   </div>
 </template>
 
@@ -29,7 +24,7 @@ export default {
     },
     data() {
         return {
-            bvid: "BV1Ts4y1872o"
+            bvid: "BV1X64y1j7Ff"
         };
     }
 }
@@ -39,5 +34,26 @@ export default {
 .main-content {
     margin-top: 80px; /* 为固定定位的导航栏留出空间 */
     padding: 20px;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.video-container {
+    margin-top: 20px;
+    position: relative;
+    width: 100%;
+    padding-bottom: 56.25%; /* 16:9 宽高比 */
+    overflow: hidden;
+    border-radius: 8px;
+}
+
+.video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
 }
 </style>
